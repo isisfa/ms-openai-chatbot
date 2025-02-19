c0af3ba1f2325168b9e63e576d2ba060f626cf91
Isis Figueiredo <isis.falves@gmail.com>
2025-02-19 16:26:43 +0000

    feat: ms-azure AI gpt-35-turbo for chat

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import ChatbotIcon from "./components/ChatbotIcon";
 import ChatForm from "./components/ChatForm";
 import ChatMessage from "./components/ChatMessage";
 import { companyInfo } from "./companyInfo"
+import { AzureOpenAI } from "openai";
 
 const App = () => {
   const [chatHistory, setChatHistory] = useState([
@@ -20,25 +21,32 @@ const App = () => {
     }
 
     // Format chat for API request
-    history = history.map(({role, text}) => ({role, parts: [{text}]}));
-
-    const requestOptions ={
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({contents: history})
-    }
+    const formattedHistory = history.map(({ role, text }) => ({
+      role: role === "model" ? "assistant" : "user",
+      content: text
+    }));
 
     try {
       // Make the API call to get the bot's response
-      const response = await fetch(import.meta.env.VITE_API_URL, requestOptions);
-      const data = await response.json();
-      if(!response.ok) throw new Error (data.error.message || "Ops, something went wrong!");
+      const client = new AzureOpenAI({
+        endpoint: "https://isis-m7aepfts-swedencentral.cognitiveservices.azure.com/",
+        apiKey: import.meta.env.VITE_AZURE_OPENAI_API_KEY,
+        apiVersion: "2024-04-01-preview",
+        // dangerouslyAllowBrowser: true
+      });
+      const response = await client.chat.completions.create({
+        model: "gpt-35-turbo",
+        messages: formattedHistory,
+        max_tokens: 100,
+        temperature: 0.7,
+      });
 
       // Clear and update chat history with bot's response
-      const apiResponseText = data.candidates[0].content.parts[0].text.replace(/\*\*(.*?)\*\*/g, "$1").trim();
+      const apiResponseText = response.choices[0].message.content.trim();
       updateHistory(apiResponseText);
-    } catch(error) {
-      updateHistory(error.message, true);
+    } catch (error) {
+      console.error("Error:", error);
+      updateHistory(error.message || "Ops, something went wrong!", true);
     }
   };
 
@@ -77,7 +85,11 @@ const App = () => {
 
         {/* Chatbot footer */}
         <div className="chat-footer">
-          <ChatForm chatHistory={chatHistory} setChatHistory={setChatHistory} generateBotResponse={generateBotResponse} />
+          <ChatForm 
+          chatHistory={chatHistory} 
+          setChatHistory={setChatHistory} 
+          generateBotResponse={generateBotResponse} 
+          />
         </div>
       </div>
     </div>
